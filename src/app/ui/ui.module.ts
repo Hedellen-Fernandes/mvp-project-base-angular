193fa58b519e6f700dd5c2bce2c5671bb3670a9a
Hedellen Fernandes <hedellenf@gmail.com>
2022-03-16 23:19:37 -0300

    Inputs and Buttons components created

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputsComponent } from './inputs/inputs.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+
+
+
+@NgModule({
+  declarations: [
+    InputsComponent,
+    ButtonsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UiModule { }
